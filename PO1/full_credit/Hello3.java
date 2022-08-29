88bec10c91e648eaf5e5710fe58d1ff447644a36
Alfredo Reina Corona <AlfredoReinaCorona@gmail.com>
2022-08-29 18:05:22 -0500

    PO1 Extreme Bonus

---
+++ PO1/full_credit/Hello3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        String name=System.getProperty("user.name");
+        System.out.println("Hello " + System.getProperty("user.name") + "!");
+    }
+    
+}
